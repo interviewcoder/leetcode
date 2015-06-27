2156078c4501335eab32b71382143dbe678b83e2
Steven Cooks <stevenjoy1113@gmail.com>
2015-06-27 10:48:59 -0400

    213 (1) break down circle

---
+++ java/src/_213_HouseRobberII/Practice.java
@@ -0,0 +1,29 @@
+/**
+ *******************************************************************************
+ * Description: 
+ * 
+ * After robbing those houses on that street, the thief has found himself 
+ * a new place for his thievery so that he will not get too much attention. 
+ * This time, all houses at this place are arranged in a circle. That means 
+ * the first house is the neighbor of the last one. Meanwhile, the security 
+ * system for these houses remain the same as for those in the previous street. 
+ * 
+ * Given a list of non-negative integers representing the amount of money
+ * of each house, determine the maximum amount of money you can rob 
+ * tonight without alerting the police.
+ * 
+ *******************************************************************************
+ * @tag : Dynamic Programming
+ * {@link https://leetcode.com/problems/house-robber-ii/  }
+ */
+package _213_HouseRobberII;
+
+/** {@link _213_HouseRobberII.PracticeTest }*/
+public class Practice {
+
+    public int rob(int[] nums) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}

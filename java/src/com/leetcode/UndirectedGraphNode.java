75f91ba8c65a8996e42def1a6a657e35848c6ed0
Steven Cooks <stevenjoy1113@gmail.com>
2015-06-26 22:23:47 -0400

    update library

--- java/src/com/leetcode/UndirectedGraphNode.java
+++ java/src/com/leetcode/UndirectedGraphNode.java
@@ -1,7 +1,10 @@
 package com.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UndirectedGraphNode {
     public int label;
@@ -10,6 +13,42 @@ public class UndirectedGraphNode {
     public UndirectedGraphNode(int x) {
         label = x;
         neighbors = new ArrayList<UndirectedGraphNode>();
+    }
+
+    public static boolean isSameGraph(UndirectedGraphNode node1,
+            UndirectedGraphNode node2) {
+        // whether two nodes have been compared
+        Map<UndirectedGraphNode, List<UndirectedGraphNode>> compareMap = new HashMap<>();
+        return isSameGraph(node1, node2, compareMap);
+    }
+
+    private static boolean isSameGraph(UndirectedGraphNode node1,
+            UndirectedGraphNode node2,
+            Map<UndirectedGraphNode, List<UndirectedGraphNode>> compareMap) {
+        if (compareMap.containsKey(node1) && compareMap.get(node1).contains(node2)) {
+            // loop up in memo
+            return true;
+        } else if (node1 == null && node2 == null) {
+            return true;
+        } else if (node1 != null && node2 != null) {
+            if (node1.label == node2.label
+                    && node1.neighbors.size() == node2.neighbors.size()) {
+                // update map
+                if (!compareMap.containsKey(node1)) {
+                    compareMap.put(node1, new ArrayList<>(Arrays.asList(node2)));
+                } else {
+                    compareMap.get(node1).add(node2);
+                }
+                for (int i = 0; i < node1.neighbors.size(); i++) {
+                    if (!isSameGraph(node1.neighbors.get(i),
+                            node2.neighbors.get(i), compareMap)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
     }
 
 }
---
+++ java/test/com/leetcode/UndirectedGraphNodeTest.java
@@ -0,0 +1,241 @@
+package com.leetcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+public class UndirectedGraphNodeTest {
+    
+    @Rule
+    public Timeout globalTimeout = new Timeout(50);
+
+    /**
+     * Test method for {@link com.leetcode.UndirectedGraphNode#isSameGraph}
+     */
+    @Test
+    public void Test0() {
+        UndirectedGraphNode n1 = null;
+        UndirectedGraphNode n2 = null;
+        assertTrue(UndirectedGraphNode.isSameGraph(n1, n2));
+    }
+
+    @Test
+    public void Test1() {
+        UndirectedGraphNode n1 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n2 = new UndirectedGraphNode(1);
+        assertTrue(UndirectedGraphNode.isSameGraph(n1, n2));
+    }
+
+    //    1         1
+    //   /_\       /_\
+    @Test
+    public void Test2() {
+        UndirectedGraphNode n1 = new UndirectedGraphNode(1);
+        n1.neighbors.add(n1);
+        UndirectedGraphNode n2 = new UndirectedGraphNode(1);
+        n2.neighbors.add(n2);
+        assertTrue(UndirectedGraphNode.isSameGraph(n1, n2));
+    }
+
+    //    1         1
+    //   /         /_\
+    //  2
+    @Test
+    public void Test3() {
+        UndirectedGraphNode n11 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n12 = new UndirectedGraphNode(2);
+        n11.neighbors.add(n12);
+        UndirectedGraphNode n21 = new UndirectedGraphNode(1);
+        n21.neighbors.add(n21);
+        assertTrue(!UndirectedGraphNode.isSameGraph(n11, n21));
+    }
+
+    //    1         1
+    //   /         /_\
+    //  2         2
+    @Test
+    public void Test4() {
+        UndirectedGraphNode n11 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n12 = new UndirectedGraphNode(2);
+        n11.neighbors.add(n12);
+        UndirectedGraphNode n21 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n22 = new UndirectedGraphNode(2);
+        n21.neighbors.add(n21);
+        n21.neighbors.add(n22);
+        assertTrue(!UndirectedGraphNode.isSameGraph(n11, n21));
+    }
+
+    //    1          1
+    //   / \        / \
+    //  2   3      2   4
+    @Test
+    public void Test5() {
+        UndirectedGraphNode n11 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n12 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n13 = new UndirectedGraphNode(3);
+        n11.neighbors.add(n12);
+        n11.neighbors.add(n13);
+        UndirectedGraphNode n21 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n22 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n23 = new UndirectedGraphNode(4);
+        n21.neighbors.add(n22);
+        n21.neighbors.add(n23);
+        assertTrue(!UndirectedGraphNode.isSameGraph(n11, n21));
+    }
+
+    //    1          1
+    //   / \        / \
+    //  2   3      2   3
+    @Test
+    public void Test6() {
+        UndirectedGraphNode n11 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n12 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n13 = new UndirectedGraphNode(3);
+        n11.neighbors.add(n12);
+        n11.neighbors.add(n13);
+        UndirectedGraphNode n21 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n22 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n23 = new UndirectedGraphNode(3);
+        n21.neighbors.add(n22);
+        n21.neighbors.add(n23);
+        assertTrue(UndirectedGraphNode.isSameGraph(n11, n21));
+    }
+
+    //    1          1
+    //   / \        / \
+    //  2 - 3      2   3
+    @Test
+    public void Test7() {
+        UndirectedGraphNode n11 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n12 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n13 = new UndirectedGraphNode(3);
+        n11.neighbors.add(n12);
+        n11.neighbors.add(n13);
+        n12.neighbors.add(n13);
+        UndirectedGraphNode n21 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n22 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n23 = new UndirectedGraphNode(3);
+        n21.neighbors.add(n22);
+        n21.neighbors.add(n23);
+        assertTrue(!UndirectedGraphNode.isSameGraph(n11, n21));
+    }
+
+    //    1          1
+    //   / \        / \
+    //  2 - 3      2 - 3
+    //   \   \          \
+    //    --- 4          4
+    @Test
+    public void Test8() {
+        UndirectedGraphNode n11 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n12 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n13 = new UndirectedGraphNode(3);
+        UndirectedGraphNode n14 = new UndirectedGraphNode(4);
+        n11.neighbors.add(n12);
+        n11.neighbors.add(n13);
+        n12.neighbors.add(n13);
+        n12.neighbors.add(n14);
+        n13.neighbors.add(n14);
+        UndirectedGraphNode n21 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n22 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n23 = new UndirectedGraphNode(3);
+        UndirectedGraphNode n24 = new UndirectedGraphNode(4);
+        n21.neighbors.add(n22);
+        n21.neighbors.add(n23);
+        n22.neighbors.add(n23);
+        n23.neighbors.add(n24);
+        assertTrue(!UndirectedGraphNode.isSameGraph(n11, n21));
+    }
+
+    //    1          1
+    //   / \        / \
+    //  2 - 3      2 - 3
+    //   \   \      \   \
+    //    --- 4      ----4
+    @Test
+    public void Test9() {
+        UndirectedGraphNode n11 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n12 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n13 = new UndirectedGraphNode(3);
+        UndirectedGraphNode n14 = new UndirectedGraphNode(4);
+        n11.neighbors.add(n12);
+        n11.neighbors.add(n13);
+        n12.neighbors.add(n13);
+        n12.neighbors.add(n14);
+        n13.neighbors.add(n14);
+        UndirectedGraphNode n21 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n22 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n23 = new UndirectedGraphNode(3);
+        UndirectedGraphNode n24 = new UndirectedGraphNode(4);
+        n21.neighbors.add(n22);
+        n21.neighbors.add(n23);
+        n22.neighbors.add(n23);
+        n22.neighbors.add(n24);
+        n23.neighbors.add(n24);
+        assertTrue(UndirectedGraphNode.isSameGraph(n11, n21));
+    }
+
+    //    1          1 --
+    //   / \        / \   \
+    //  2 - 3      2 - 3   \
+    //   \   \      \   \   \
+    //    --- 4       ---4 --
+    @Test
+    public void Test10() {
+        UndirectedGraphNode n11 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n12 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n13 = new UndirectedGraphNode(3);
+        UndirectedGraphNode n14 = new UndirectedGraphNode(4);
+        n11.neighbors.add(n12);
+        n11.neighbors.add(n13);
+        n12.neighbors.add(n13);
+        n12.neighbors.add(n14);
+        n13.neighbors.add(n14);
+        UndirectedGraphNode n21 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n22 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n23 = new UndirectedGraphNode(3);
+        UndirectedGraphNode n24 = new UndirectedGraphNode(4);
+        n21.neighbors.add(n22);
+        n21.neighbors.add(n23);
+        n22.neighbors.add(n23);
+        n22.neighbors.add(n24);
+        n23.neighbors.add(n24);
+        n24.neighbors.add(n21);
+        assertTrue(!UndirectedGraphNode.isSameGraph(n11, n21));
+    }
+
+    //    1          1
+    //   / \        / \
+    //  2 - 3      2 - 3
+    //   \   \      \   \
+    //    --- 4      ----4
+    //                  /
+    //                 5
+    @Test
+    public void Test11() {
+        UndirectedGraphNode n11 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n12 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n13 = new UndirectedGraphNode(3);
+        UndirectedGraphNode n14 = new UndirectedGraphNode(4);
+        n11.neighbors.add(n12);
+        n11.neighbors.add(n13);
+        n12.neighbors.add(n13);
+        n12.neighbors.add(n14);
+        n13.neighbors.add(n14);
+        UndirectedGraphNode n21 = new UndirectedGraphNode(1);
+        UndirectedGraphNode n22 = new UndirectedGraphNode(2);
+        UndirectedGraphNode n23 = new UndirectedGraphNode(3);
+        UndirectedGraphNode n24 = new UndirectedGraphNode(4);
+        UndirectedGraphNode n25 = new UndirectedGraphNode(5);
+        n21.neighbors.add(n22);
+        n21.neighbors.add(n23);
+        n22.neighbors.add(n23);
+        n22.neighbors.add(n24);
+        n23.neighbors.add(n24);
+        n24.neighbors.add(n25);
+        assertTrue(!UndirectedGraphNode.isSameGraph(n11, n21));
+    }
+
+}

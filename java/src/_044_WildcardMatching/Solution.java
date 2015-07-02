0442c6ed5bf663bbb7b348936fa2a71215795d0a
Steven Cooks <stevenjoy1113@gmail.com>
2015-07-02 16:36:22 -0400

    044 (1) dp + backtracking!

---
+++ java/src/_044_WildcardMatching/Solution.java
@@ -0,0 +1,69 @@
+/**
+ * Time : O(); Space : O()
+ * @tag : Dynamic Programming; Backtracking; Greedy; String
+ * @by  : Steven Cooks
+ * @date: Jul 2, 2015
+ *************************************************************************
+ * Description: 
+ * 
+ * Implement wildcard pattern matching with support for '?' and '*'. 
+ * 
+ * '?' Matches any single character. 
+ * '*' Matches any sequence of characters (including the empty sequence). 
+ * 
+ * The matching should cover the entire input string (not partial). 
+ * 
+ * The function prototype should be: 
+ * bool isMatch(const char *s, const char *p) 
+ * 
+ * Some examples: 
+ * 
+ *  isMatch("aa","a") → false 
+ *  isMatch("aa","aa") → true 
+ *  isMatch("aaa","aa") → false 
+ *  isMatch("aa", "*") → true 
+ *  isMatch("aa", "a*") → true 
+ *  isMatch("ab", "?*") → true 
+ *  isMatch("aab", "c*a*b") → false
+ * 
+ *************************************************************************
+ * {@link https://leetcode.com/problems/wildcard-matching/ }
+ */
+package _044_WildcardMatching;
+
+/** see test {@link _044_WildcardMatching.SolutionTest } */
+public class Solution {
+    
+    public boolean isMatch(String s, String p) {
+        int sLen = s.length();
+        int pLen = p.length();
+        int sIndex = 0;
+        int pIndex = 0;
+        int index = 0;
+        int startIndex = -1;
+        while (sIndex < sLen) {
+            if (pIndex < pLen && (p.charAt(pIndex) == '?' || s.charAt(sIndex) == p.charAt(pIndex))) {
+                // advance index on s and p together
+                sIndex++;
+                pIndex++;
+            } else if (pIndex < pLen && p.charAt(pIndex) == '*') {
+                // 
+                startIndex = pIndex;
+                index = sIndex;
+                pIndex++;
+            } else if (startIndex != -1) {
+                // 
+                pIndex = startIndex + 1;
+                index++;
+                sIndex = index;
+            } else {
+                return false;
+            }
+        }
+        while (pIndex < pLen && p.charAt(pIndex) == '*') {
+            pIndex++;
+        }
+        return pIndex == pLen;
+    }
+
+}
---
+++ java/src/_044_WildcardMatching/SolutionTLE.java
@@ -0,0 +1,49 @@
+/**
+ * Time : O(); Space : O()
+ * @tag : Dynamic Programming; Backtracking; Greedy; String
+ * @by  : Steven Cooks
+ * @date: Jul 2, 2015
+ */
+package _044_WildcardMatching;
+
+/** see test {@link _044_WildcardMatching.SolutionTLETest } */
+public class SolutionTLE {
+
+    public boolean isMatch(String s, String p) {
+        return isMatchCore(s, p, 0, 0, s.length(), p.length());
+    }
+
+    private boolean isMatchCore(String s, String p, int sIndex, int pIndex,
+            int sLen, int pLen) {
+        // base case
+        if (sIndex >= s.length() && pIndex == p.length()) {
+            // both string and pattern end
+            return true;
+        }
+        if (sIndex < s.length() && pIndex == p.length()) {
+            // string not ends, but pattern ends
+            return false;
+        }
+
+        // recursive case 1: met '*' in pattern
+        if (p.charAt(pIndex) == '*') {
+            boolean b1 = isMatchCore(s, p, sIndex, pIndex + 1, sLen, pLen);
+            boolean b2 = sIndex < sLen
+                    && isMatchCore(s, p, sIndex + 1, pIndex, sLen, pLen);
+            return b1 || b2;
+            // use '*' to match empty sequence in string
+            // return isMatchCore(s, p, sIndex, pIndex + 1, sLen, pLen)
+            // use '*' to match a sequence including s.charAt(sIndex) in string
+            // || isMatchCore(s, p, sIndex + 1, pIndex, sLen, pLen);
+        }
+        // recursive case 2: met character other than '*' in pattern
+        if (sIndex < sLen
+                && (s.charAt(sIndex) == p.charAt(pIndex) || p.charAt(pIndex) == '?')) {
+            // met same characters in string and pattern || met '?' in pattern
+            return isMatchCore(s, p, sIndex + 1, pIndex + 1, sLen, pLen);
+        }
+
+        return false;
+    }
+
+}

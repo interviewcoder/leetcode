c5cd8871f0343b285b7afb464a74f6b8d9f9086f
Steven Cooks <stevenjoy1113@gmail.com>
2015-07-02 18:46:51 -0400

    010 (1) dp + backtracking

---
+++ java/src/_010_RegularExpressionMatching/Solution.java
@@ -0,0 +1,89 @@
+/**
+ * Time : O(); Space : O()
+ * @tag : Dynamic Programming; Backtracking; String
+ * @by  : Steven Cooks
+ * @date: Jul 2, 2015
+ *******************************************************************************
+ * Description: 
+ * 
+ * Implement regular expression matching with support for '.' and '*'. 
+ * '.' Matches any single character. 
+ * '*' Matches zero or more of the preceding element. 
+ * The matching should cover the entire input string (not partial). 
+ * 
+ * The function prototype should be: 
+ * 
+ * bool isMatch(const char *s, const char *p) 
+ * 
+ * Some examples: 
+ * 
+ * isMatch("aa","a") → false 
+ * isMatch("aa","aa") → true 
+ * isMatch("aaa","aa") → false 
+ * isMatch("aa", "a*") → true 
+ * isMatch("aa", ".*") → true 
+ * isMatch("ab", ".*") → true 
+ * isMatch("aab", "c*a*b") → true
+ * 
+ *******************************************************************************
+ * {@link https://leetcode.com/problems/regular-expression-matching/ }
+ */
+package _010_RegularExpressionMatching;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/** see test {@link _010_RegularExpressionMatching.SolutionTest } */
+public class Solution {
+
+    Map<Integer, Map<Integer, Boolean>> memo = new HashMap<>();
+
+    public boolean isMatch(String s, String p) {
+        int sLen = s.length();
+        int pLen = p.length();
+        int sIndex = 0;
+        int pIndex = 0;
+        return isMatchCore(s, p, sIndex, pIndex, sLen, pLen);
+    }
+
+    private boolean isMatchCore(String s, String p, int sIndex, int pIndex,
+            int sLen, int pLen) {
+        // look up in memo
+        if (memo.containsKey(sIndex) && memo.get(sIndex).containsKey(pIndex)) {
+            return memo.get(sIndex).get(pIndex);
+        }
+
+        if (sIndex == sLen && pIndex == pLen) {
+            return true;
+        }
+        if (sIndex != sLen && pIndex == pLen) {
+            return false;
+        }
+        boolean matched = false;
+        if ((pIndex + 1) < pLen && p.charAt(pIndex + 1) == '*') {
+            if (sIndex < sLen && p.charAt(pIndex) == s.charAt(sIndex)
+                    || (p.charAt(pIndex) == '.' && sIndex < sLen)) {
+                // if equals or met with '.', then move on to the next state
+                matched = isMatchCore(s, p, sIndex + 1, pIndex + 2, sLen, pLen)
+                        || isMatchCore(s, p, sIndex + 1, pIndex, sLen, pLen)
+                        || isMatchCore(s, p, sIndex, pIndex + 2, sLen, pLen);
+            } else {
+                // ignore a '*'
+                matched = isMatchCore(s, p, sIndex, pIndex + 2, sLen, pLen);
+            }
+        } else if (sIndex < sLen && pIndex < pLen
+                && s.charAt(sIndex) == p.charAt(pIndex)
+                || (p.charAt(pIndex) == '.' && sIndex < sLen)) {
+            matched = isMatchCore(s, p, sIndex + 1, pIndex + 1, sLen, pLen);
+        }
+        if (!memo.containsKey(sIndex)) {
+            Map<Integer, Boolean> map = new HashMap<>();
+            map.put(pIndex, matched);
+            memo.put(sIndex, map);
+        } else {
+            memo.get(sIndex).put(pIndex, matched);
+        }
+        return matched;
+    }
+
+}
---
+++ java/test/_010_RegularExpressionMatching/PracticeTest.java
@@ -0,0 +1,134 @@
+package _010_RegularExpressionMatching;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+public class PracticeTest {
+    
+    /** Test method for {@link _010_RegularExpressionMatching.Practice } */
+    Practice solution;
+
+    @Rule
+    public Timeout globalTimeout = new Timeout(20);
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Practice();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    @Test
+    public void Test1() {
+        String s = "aa";
+        String p = "a";
+        assertTrue(!solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test2() {
+        String s = "aa";
+        String p = "aa";
+        assertTrue(solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test3() {
+        String s = "aaa";
+        String p = "aa";
+        assertTrue(!solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test4() {
+        String s = "aa";
+        String p = "a*";
+        assertTrue(solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test5() {
+        String s = "aa";
+        String p = ".*";
+        assertTrue(solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test6() {
+        String s = "ab";
+        String p = ".*";
+        assertTrue(solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test7() {
+        String s = "aab";
+        String p = "c*a*b";
+        assertTrue(solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test8() {
+        String s = "aaa";
+        String p = "a.a";
+        assertTrue(solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test9() {
+        String s = "aaa";
+        String p = "ab*ac*a";
+        assertTrue(solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test10() {
+        String s = "aaa";
+        String p = "aa.a";
+        assertTrue(!solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test11() {
+        String s = "aaa";
+        String p = "ab*a";
+        assertTrue(!solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test12() {
+        String s = "aaa";
+        String p = "*a";
+        assertTrue(!solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test13() {
+        String s = "ab";
+        String p = ".*c";
+        assertTrue(!solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test14() {
+        String s = "";
+        String p = ".*";
+        assertTrue(solution.isMatch(s, p));
+    }
+
+    @Test
+    public void Test15() {
+        String s = "baccbbcbcacacbbc";
+        String p = "c*.*b*c*ba*b*b*.a*";
+        assertTrue(solution.isMatch(s, p));
+    }
+
+}

7fcb34482da771d32bdaaf99f5dffee7bc4250a0
Steven Cooks <stevenjoy1113@gmail.com>
2015-07-15 23:06:11 -0400

    042 (1) Two pointer towards center

---
+++ java/src/_042_TrappingRainWater/Practice.java
@@ -0,0 +1,26 @@
+/**
+ *************************************************************************
+ * Description: 
+ * 
+ * Given an unsorted integer array, find the first missing positive integer. 
+ * 
+ * For example, Given [1,2,0] return 3, 
+ * and [3,4,-1,1] return 2. 
+ * 
+ * Your algorithm should run in O(n) time and uses constant space.
+ * 
+ *************************************************************************
+ * @tag : Array; Stack; Two Pointers
+ * {@link https://leetcode.com/problems/trapping-rain-water/ }
+ */
+package _042_TrappingRainWater;
+
+/** see test {@link _042_TrappingRainWater.PracticeTest } */
+public class Practice {
+
+    public int trap(int[] height) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}

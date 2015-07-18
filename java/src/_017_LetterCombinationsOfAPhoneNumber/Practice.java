f4e230214d704b0537901123958881c809a6ea3b
Steven Cooks <stevenjoy1113@gmail.com>
2015-07-18 09:49:33 -0400

    017 (1) correct Camel Style name

---
+++ java/src/_017_LetterCombinationsOfAPhoneNumber/Practice.java
@@ -0,0 +1,33 @@
+/**
+ *************************************************************************
+ * Description: 
+ * 
+ * Given a digit string, return all possible letter combinations that the 
+ * number could represent. 
+ * A mapping of digit to letters (just like on the telephone buttons) 
+ * is given below.
+ * 
+ * Input:Digit string "23" 
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]. 
+ * 
+ * Note: 
+ * Although the above answer is in lexicographical order, your answer 
+ * could be in any order you want.
+ * 
+ *************************************************************************
+ * @tag : Backtracking; String
+ * {@link https://leetcode.com/problems/3sum-closest/ }
+ */
+package _017_LetterCombinationsOfAPhoneNumber;
+
+import java.util.List;
+
+/** see test {@link _017_LetterCombinationsOfAPhoneNumber.PracticeTest } */
+public class Practice {
+
+    public List<String> letterCombinations(String digits) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}

272e526f347490eb0187ee437bf34f73e9c6eb4b
Steven Cooks <stevenjoy1113@gmail.com>
2015-07-20 22:22:23 -0400

    update Progress

--- java/src/com/leetcode/Progress.java
+++ java/src/com/leetcode/Progress.java
@@ -1,3 +1,13 @@
+/**
+ * File: src/com/leetcode/Progress.java
+ * -----------------------------------------------------------------------
+ * Generate a mark down file that recording solutions in my git repository.
+ * 
+ * Run below command in terminal first: (which creates input file)
+ *      $ sh parse
+ *      
+ * Dependencies: join.awk, parse.sed, parse      
+ */
 package com.leetcode;
 
 import java.io.BufferedReader;
@@ -15,30 +25,34 @@ import java.util.Set;
 import javax.net.ssl.HttpsURLConnection;
 
 public class Progress {
-    
+
     private static final String DELIMITER = "\t";
 
-    private static final String INPUT_FILE = "scripts/leetcode.tsv";
-    private static final String DEST_FILE = "problems.md";
+    private static final String INPUT_FILE = "scripts/progress.tsv";
+    private static final String DEST_FILE = "scripts/progress.md";
 
     private static final String GIT_PREFIX = "https://github.com/interviewcoder/leetcode/blob/master/java/src/";
     private static final String GIT_SUFFIX = "/Solution.java";
     
+    private int undone = 0;
+
     @SuppressWarnings("serial")
-    private static final Set<Character> skipSet = new HashSet<Character>(){{
-        add(')');
-        add('(');
-        add(' ');
-        add(',');
-        add('-');
-        add('\'');
-    }};
-    
+    private static final Set<Character> skipSet = new HashSet<Character>() {
+        {
+            add(')');
+            add('(');
+            add(' ');
+            add(',');
+            add('-');
+            add('\'');
+        }
+    };
+
     public static void main(String[] args) {
         new Progress().run();
     }
 
-    private void run() {
+    public void run() {
         List<String> page = readInputFile();
         writeMDFile(page);
         System.out.println("done!");
@@ -53,16 +67,25 @@ public class Progress {
                 String[] cols = page.get(i).split(DELIMITER);
                 StringBuilder row = new StringBuilder();
                 for (int j = 0; j < cols.length; j++) {
-                    row.append("|");
-                    if (i != 0 && j == 1) {
-                        // try to find active url for this problem
-                        row.append(toGitURL(cols[0], cols[j]));
-                    } else {
-                        row.append(cols[j]); 
-                    }
-                    if (j == cols.length - 1) {
+                    // skip the acceptance column
+                    if (j != 2) {
                         row.append("|");
+                        if (i != 0 && j == 1) {
+                            // try to find active url for this problem
+                            row.append(toGitURL(cols[0], cols[j]));
+                        } else {
+                            row.append(cols[j]);
+                        }
+                        if (j == cols.length - 1) {
+                            row.append("|");
+                        }
                     }
+                }
+                // add tags and description
+                if (i == 0) {
+                    row.append( " Tags | Description | ");
+                } else {
+                    row.append(" Array |     |");
                 }
                 bw.write(row.toString());
                 bw.newLine();
@@ -74,6 +97,7 @@ public class Progress {
                 }
             }
             bw.close();
+            System.out.println("undone: " + undone +  " out of " + (page.size() - 2));
         } catch (IOException e) {
             System.err.println(e);
         }
@@ -96,7 +120,8 @@ public class Progress {
                 }
             }
         }
-        String urlStr = GIT_PREFIX + "_" + getGitNumber(num) +  "_" + name.toString() + GIT_SUFFIX;
+        String urlStr = GIT_PREFIX + "_" + getGitNumber(num) + "_"
+                + name.toString() + GIT_SUFFIX;
         try {
             URL url;
             url = new URL(urlStr);
@@ -104,10 +129,11 @@ public class Progress {
             huc.setRequestMethod("HEAD");
             if (huc.getResponseCode() == HttpsURLConnection.HTTP_OK) {
                 return "[" + probName + "](" + urlStr + ")";
-            } 
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        undone++;
         System.out.println(urlStr);
         return probName;
     }

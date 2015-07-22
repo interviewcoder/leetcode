74c844f931bc3107c181ae6500e48d65d0ebe1fe
Steven Cooks <stevenjoy1113@gmail.com>
2015-07-21 23:26:04 -0400

    061 (2) update tests

--- java/test/_061_RotateList/PracticeTest.java
+++ java/test/_061_RotateList/PracticeTest.java
@@ -15,134 +15,104 @@ public class PracticeTest {
     /** Test method for {@link _061_RotateList.Practice } */
     Practice solution;
 
-    ListNode head;
-
     @Rule
     public Timeout globalTimeout = new Timeout(20);
 
     @Before
     public void setUp() throws Exception {
         solution = new Practice();
-        // List: 1->2->3->4->5
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
-        ListNode node5 = new ListNode(5);
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        head = node1;
     }
 
     @After
     public void tearDown() throws Exception {
         solution = null;
-        head = null;
     }
 
     @Test
     public void Test1() {
         int k = 2;
+        int[] nums = {1, 2, 3, 4, 5};
+        ListNode head = ListNode.constructLinkedList(nums);
         ListNode actual = solution.rotateRight(head, k);
-        ListNode expect1 = new ListNode(4);
-        ListNode expect2 = new ListNode(5);
-        ListNode expect3 = new ListNode(1);
-        ListNode expect4 = new ListNode(2);
-        ListNode expect5 = new ListNode(3);
-        expect1.next = expect2;
-        expect2.next = expect3;
-        expect3.next = expect4;
-        expect4.next = expect5;
-        ListNode expected = expect1;
+        int[] exps = {4, 5, 1, 2, 3};
+        ListNode expected = ListNode.constructLinkedList(exps);
         assertTrue(ListNode.isSameList(actual, expected));
     }
 
     @Test
     public void Test2() {
         int k = 1;
+        int[] nums = {1, 2, 3, 4, 5};
+        ListNode head = ListNode.constructLinkedList(nums);
         ListNode actual = solution.rotateRight(head, k);
-        ListNode expect1 = new ListNode(5);
-        ListNode expect2 = new ListNode(1);
-        ListNode expect3 = new ListNode(2);
-        ListNode expect4 = new ListNode(3);
-        ListNode expect5 = new ListNode(4);
-        expect1.next = expect2;
-        expect2.next = expect3;
-        expect3.next = expect4;
-        expect4.next = expect5;
-        ListNode expected = expect1;
+        int[] exps = {5, 1, 2, 3, 4};
+        ListNode expected = ListNode.constructLinkedList(exps);
         assertTrue(ListNode.isSameList(actual, expected));
     }
 
     @Test
     public void Test3() {
         int k = 3;
+        int[] nums = {1, 2, 3, 4, 5};
+        ListNode head = ListNode.constructLinkedList(nums);
         ListNode actual = solution.rotateRight(head, k);
-        ListNode expect1 = new ListNode(3);
-        ListNode expect2 = new ListNode(4);
-        ListNode expect3 = new ListNode(5);
-        ListNode expect4 = new ListNode(1);
-        ListNode expect5 = new ListNode(2);
-        expect1.next = expect2;
-        expect2.next = expect3;
-        expect3.next = expect4;
-        expect4.next = expect5;
-        ListNode expected = expect1;
+        int[] exps = {3, 4, 5, 1, 2};
+        ListNode expected = ListNode.constructLinkedList(exps);
         assertTrue(ListNode.isSameList(actual, expected));
     }
 
     @Test
     public void Test4() {
         int k = 5;
+        int[] nums = {1, 2, 3, 4, 5};
+        ListNode head = ListNode.constructLinkedList(nums);
         ListNode actual = solution.rotateRight(head, k);
-        ListNode expect1 = new ListNode(1);
-        ListNode expect2 = new ListNode(2);
-        ListNode expect3 = new ListNode(3);
-        ListNode expect4 = new ListNode(4);
-        ListNode expect5 = new ListNode(5);
-        expect1.next = expect2;
-        expect2.next = expect3;
-        expect3.next = expect4;
-        expect4.next = expect5;
-        ListNode expected = expect1;
-        // System.out.println(ListNode.toString(expected));
-        // System.out.println(ListNode.toString(actual));
+        int[] exps = {1, 2, 3, 4, 5};
+        ListNode expected = ListNode.constructLinkedList(exps);
         assertTrue(ListNode.isSameList(actual, expected));
     }
 
     @Test
     public void Test5() {
         int k = 6;
+        int[] nums = {1, 2, 3, 4, 5};
+        ListNode head = ListNode.constructLinkedList(nums);
         ListNode actual = solution.rotateRight(head, k);
-        ListNode expect1 = new ListNode(5);
-        ListNode expect2 = new ListNode(1);
-        ListNode expect3 = new ListNode(2);
-        ListNode expect4 = new ListNode(3);
-        ListNode expect5 = new ListNode(4);
-        expect1.next = expect2;
-        expect2.next = expect3;
-        expect3.next = expect4;
-        expect4.next = expect5;
-        ListNode expected = expect1;
+        int[] exps = {5, 1, 2, 3, 4};
+        ListNode expected = ListNode.constructLinkedList(exps);
         assertTrue(ListNode.isSameList(actual, expected));
     }
 
     @Test
     public void Test6() {
         int k = 6;
+        int[] nums = {};
+        ListNode head = ListNode.constructLinkedList(nums);
         ListNode actual = solution.rotateRight(head, k);
-        ListNode expect1 = new ListNode(5);
-        ListNode expect2 = new ListNode(1);
-        ListNode expect3 = new ListNode(2);
-        ListNode expect4 = new ListNode(3);
-        ListNode expect5 = new ListNode(4);
-        expect1.next = expect2;
-        expect2.next = expect3;
-        expect3.next = expect4;
-        expect4.next = expect5;
-        ListNode expected = expect1;
+        int[] exps = {};
+        ListNode expected = ListNode.constructLinkedList(exps);
+        assertTrue(ListNode.isSameList(actual, expected));
+    }
+
+    @Test
+    public void Test7() {
+        int k = 0;
+        int[] nums = {};
+        ListNode head = ListNode.constructLinkedList(nums);
+        ListNode actual = solution.rotateRight(head, k);
+        int[] exps = {};
+        ListNode expected = ListNode.constructLinkedList(exps);
+        assertTrue(ListNode.isSameList(actual, expected));
+    }
+
+    @Test
+    public void Test8() {
+        int k = 1;
+        int[] nums = {1};
+        ListNode head = ListNode.constructLinkedList(nums);
+        ListNode actual = solution.rotateRight(head, k);
+        int[] exps = {1};
+        ListNode expected = ListNode.constructLinkedList(exps);
         assertTrue(ListNode.isSameList(actual, expected));
     }
 

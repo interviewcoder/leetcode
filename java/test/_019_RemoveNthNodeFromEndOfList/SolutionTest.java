596c8d2c4ce2c5509cdaed5aa6c040d97fcff112
Steven Cooks <stevenjoy1113@gmail.com>
2015-07-21 23:31:00 -0400

    019 (2) update tests

--- java/test/_019_RemoveNthNodeFromEndOfList/SolutionTest.java
+++ java/test/_019_RemoveNthNodeFromEndOfList/SolutionTest.java
@@ -15,94 +15,73 @@ public class SolutionTest {
     /** Test method for {@link _019_RemoveNthNodeFromEndOfList.Solution } */
     Solution solution;
 
-    ListNode head;
-
     @Rule
     public Timeout globalTimeout = new Timeout(20);
 
     @Before
     public void setUp() throws Exception {
         solution = new Solution();
-        // List: 1->2->3->4->5
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
-        ListNode node5 = new ListNode(5);
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-
-        head = node1;
     }
 
     @After
     public void tearDown() throws Exception {
         solution = null;
-        head = null;
     }
 
     @Test
     public void Test1() {
         int n = 2;
+        int[] nums = {1, 2, 3, 4, 5};
+        ListNode head = ListNode.constructLinkedList(nums);
         ListNode actual = solution.removeNthFromEnd(head, n);
-
         // 1->2->3->5
-        ListNode expect1 = new ListNode(1);
-        ListNode expect2 = new ListNode(2);
-        ListNode expect3 = new ListNode(3);
-        ListNode expect4 = new ListNode(5);
-        expect1.next = expect2;
-        expect2.next = expect3;
-        expect3.next = expect4;
-
-        ListNode expect = expect1;
-        // System.out.println(ListNode.toString(actual));
-        // System.out.println(ListNode.toString(expect));
-
+        int[] exps = {1, 2, 3, 5};
+        ListNode expect = ListNode.constructLinkedList(exps);
         assertTrue(ListNode.isSameList(actual, expect));
     }
 
     @Test
     public void Test2() {
         int n = 5;
+        int[] nums = {1, 2, 3, 4, 5};
+        ListNode head = ListNode.constructLinkedList(nums);
         ListNode actual = solution.removeNthFromEnd(head, n);
-
         // 2->3->4->5
-        ListNode expect1 = new ListNode(2);
-        ListNode expect2 = new ListNode(3);
-        ListNode expect3 = new ListNode(4);
-        ListNode expect4 = new ListNode(5);
-        expect1.next = expect2;
-        expect2.next = expect3;
-        expect3.next = expect4;
-
-        ListNode expect = expect1;
-        // System.out.println(ListNode.toString(actual));
-        // System.out.println(ListNode.toString(expect));
-
+        int[] exps = {2, 3, 4, 5};
+        ListNode expect = ListNode.constructLinkedList(exps);
         assertTrue(ListNode.isSameList(actual, expect));
     }
 
     @Test
     public void Test3() {
         int n = 1;
+        int[] nums = {1, 2, 3, 4, 5};
+        ListNode head = ListNode.constructLinkedList(nums);
         ListNode actual = solution.removeNthFromEnd(head, n);
+        int[] exps = {1, 2, 3, 4};
+        ListNode expect = ListNode.constructLinkedList(exps);
+        assertTrue(ListNode.isSameList(actual, expect));
+    }
 
-        // 1->2->3->4
-        ListNode expect1 = new ListNode(1);
-        ListNode expect2 = new ListNode(2);
-        ListNode expect3 = new ListNode(3);
-        ListNode expect4 = new ListNode(4);
-        expect1.next = expect2;
-        expect2.next = expect3;
-        expect3.next = expect4;
+    @Test
+    public void Test4() {
+        int n = 0;
+        int[] nums = {};
+        ListNode head = ListNode.constructLinkedList(nums);
+        ListNode actual = solution.removeNthFromEnd(head, n);
+        int[] exps = {};
+        ListNode expect = ListNode.constructLinkedList(exps);
+        assertTrue(ListNode.isSameList(actual, expect));
+    }
 
-        ListNode expect = expect1;
-        // System.out.println(ListNode.toString(actual));
-        // System.out.println(ListNode.toString(expect));
-
+    @Test
+    public void Test5() {
+        int n = 1;
+        int[] nums = {1};
+        ListNode head = ListNode.constructLinkedList(nums);
+        ListNode actual = solution.removeNthFromEnd(head, n);
+        int[] exps = {};
+        ListNode expect = ListNode.constructLinkedList(exps);
         assertTrue(ListNode.isSameList(actual, expect));
     }
 

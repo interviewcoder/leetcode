717134c37cad92d33b6c20d3f8471de73ab43641
Steven Cooks <stevenjoy1113@gmail.com>
2015-08-09 01:55:46 -0400

    s05 (1) memo DP + BFS

--- java/src/s05_LongestIncreasingContinuousSubsequenceII/Solution.java
+++ java/src/s05_LongestIncreasingContinuousSubsequenceII/Solution.java
@@ -1,5 +1,5 @@
 /**
- * Time : O() ; Space: O()
+ * Time : O(m*n) ; Space: O(m*n)
  * @tag : 
  * @by  : Steven Cooks
  * @date: Aug 7, 2015
@@ -23,14 +23,20 @@
  * 
  ***************************************************************************
  * {@link http://www.lintcode.com/en/problem/longest-increasing-continuous-subsequence-ii/ }
+ * see also [POJ][1088]
  */
 package s05_LongestIncreasingContinuousSubsequenceII;
 
 /** see test {@link s05_LongestIncreasingContinuousSubsequenceII.SolutionTest } */
 public class Solution {
-    
+
+    // memo[i][j] is the count of numbers in the longest increasing continuous 
+    // subsequence that starting from position A[i][j]
     private int[][] memo;
-    
+
+    /**
+     * memo DP, search for path using BFS and memorize path along the way. 
+     */
     public int longestIncreasingContinuousSubsequenceII(int[][] A) {
         if (A.length == 0 || A[0].length == 0) {
             return 0;
@@ -38,25 +44,41 @@ public class Solution {
         int rows = A.length;
         int cols = A[0].length;
         memo = new int[rows + 1][cols + 1];
+        boolean[][] visited = new boolean[rows + 1][cols + 1];
         int result = 0;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                int count = findIncreasingPath(i, j, A);
+                // since we want rigidly increasing path, we set start number
+                // as A[i][j] - 1 so that (i, j) can be the starting point
+                int count = findIncreasingPath(A[i][j] - 1, i, j, rows, cols,  A, visited);
                 result = Math.max(result, count);
             }
         }
         return result;
     }
 
-    private int findIncreasingPath(int i, int j, int[][] matrix) {
-        // base case
-        if (memo[i][j] != 0) {
+    // find longest increasing path that begins from matrix[i][j], 
+    // and the starting point should have larger number than `start`
+    private int findIncreasingPath(int start, int i, int j, int rows, int cols, int[][] matrix,
+            boolean[][] visited) {
+        // base case: check within matrix's bounds, check path is increasing, and check memo
+        if (i < 0 || i >= rows || j < 0 || j >= cols || visited[i][j] || matrix[i][j] <= start) {
+            return 0;
+        } else if (memo[i][j] != 0) {
             return memo[i][j];
         }
+        // recursive case: find longest path from neighbors
         int count = 0;
-
-        memo[i][j] = count;
-        return count;
+        visited[i][j] = true;
+        int num = matrix[i][j];
+        count = Math.max(count, findIncreasingPath(num, i - 1, j, rows, cols, matrix, visited));
+        count = Math.max(count, findIncreasingPath(num, i + 1, j, rows, cols, matrix, visited));
+        count = Math.max(count, findIncreasingPath(num, i, j - 1, rows, cols, matrix, visited));
+        count = Math.max(count, findIncreasingPath(num, i, j + 1, rows, cols, matrix, visited));
+        int result = 1 + count;
+        memo[i][j] = result;
+        visited[i][j] = false;
+        return result;
     }
 
 }
--- java/src/s05_LongestIncreasingContinuousSubsequenceII/SolutionTest.java
+++ java/src/s05_LongestIncreasingContinuousSubsequenceII/SolutionTest.java
@@ -17,7 +17,7 @@ public class SolutionTest {
     Solution solution;
 
     @Rule
-    public Timeout globalTimeout = new Timeout(200);
+    public Timeout globalTimeout = new Timeout(2000);
 
     @Before
     public void setUp() throws Exception {
@@ -40,6 +40,44 @@ public class SolutionTest {
         };
         int actual = solution.longestIncreasingContinuousSubsequenceII(A);
         int expected = 25;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test2() {
+        int[][] A = {
+                {16,17,24,23,6},
+                {15,18,25,22,7},
+                {1 ,2 ,3, 4 ,5},
+                {14,19,10, 7,6},
+                {13,12, 9, 8,9}
+        };
+        int actual = solution.longestIncreasingContinuousSubsequenceII(A);
+        int expected = 13;
+        assertEquals(expected, actual);
+    }
+
+    //  0,1,. ...  9
+    //  19,18,    10
+    //  20,21,....29
+    //  39,38,... 30
+    //      ....
+    @Test
+    public void Test3() {
+        int nums = 10000;
+        int rows = (int) Math.sqrt(nums);
+        int[][] A = new int[rows][rows];
+        boolean odd = true;
+        int num = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rows; j++) {
+                int col = odd ? j : (rows - 1 - j);
+                A[i][col] = num++;
+            }
+            odd = !odd;
+        }
+        int actual = solution.longestIncreasingContinuousSubsequenceII(A);
+        int expected = nums;
         assertEquals(expected, actual);
     }
 

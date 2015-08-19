b62f624a0539e42d302507ee0de77ac444965fde
Steven Cooks <stevenjoy1113@gmail.com>
2015-08-19 17:17:46 -0400

    238 (1) cache result

---
+++ java/src/_238_ProductOfArrayExceptSelf/Solution.java
@@ -0,0 +1,46 @@
+/**
+ * Time : O(N) ; Space: O(1)
+ * @tag : Array
+ * @by  : Steven Cooks
+ * @date: Aug 19, 2015
+ ***************************************************************************
+ * Description:
+ *
+ * Given an array of n integers where n > 1, nums, return an array output 
+ * such that output[i] is equal to the product of all the elements of nums 
+ * except nums[i]. 
+ * 
+ * Solve it without division and in O(n). 
+ * 
+ * For example, given [1,2,3,4], return [24,12,8,6]. 
+ * 
+ * Follow up: 
+ * Could you solve it with constant space complexity? (Note: The output array
+ * does not count as extra space for the purpose of space complexity analysis.)
+ * 
+ ***************************************************************************
+ * {@link https://leetcode.com/problems/product-of-array-except-self/ }
+ */
+package _238_ProductOfArrayExceptSelf;
+
+/** see test {@link _238_ProductOfArrayExceptSelf.SolutionTest } */
+public class Solution {
+
+    // cache product of left subarray
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int left = 1;
+        for (int i = 0; i < n; i++) {
+            res[i] = left; 
+            left *= nums[i];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+
+}
---
+++ java/test/_238_ProductOfArrayExceptSelf/SolutionLinearSpaceTest.java
@@ -0,0 +1,45 @@
+package _238_ProductOfArrayExceptSelf;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+public class SolutionLinearSpaceTest {
+    
+    /** Test method for {@link _238_ProductOfArrayExceptSelf.SolutionLinearSpace } */
+    Solution solution;
+
+    @Rule
+    public Timeout globalTimeout = new Timeout(200);
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    @Test
+    public void Test1() {
+        int[] nums = {1, 2, 3, 4};
+        int[] actuals = solution.productExceptSelf(nums);
+        int[] expecteds = new int[]{24, 12, 8, 6};
+        assertArrayEquals(expecteds, actuals);
+    }
+
+    @Test
+    public void Test2() {
+        int[] nums = {1, 2};
+        int[] actuals = solution.productExceptSelf(nums);
+        int[] expecteds = new int[]{2, 1};
+        assertArrayEquals(expecteds, actuals);
+    }
+
+}

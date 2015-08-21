d5004bf1796736cda0cb73453ee0bbd5a824cf9a
Steven Cooks <stevenjoy1113@gmail.com>
2015-08-21 10:07:42 -0400

    232 (1) queue <- two stacks

---
+++ java/src/_232_ImplementQueueUsingStack/Solution.java
@@ -0,0 +1,70 @@
+/**
+ * Time : O() ; Space: O()
+ * @tag : Stack; Data Structure
+ * @by  : Steven Cooks
+ * @date: Aug 21, 2015
+ ***************************************************************************
+ * Description:
+ * 
+ * Implement the following operations of a queue using stacks. 
+ *  push(x) -- Push element x to the back of queue.
+ *  pop()   -- Removes the element from in front of queue.
+ *  peek()  -- Get the front element.
+ *  empty() -- Return whether the queue is empty.
+ * Notes: 
+ * You must use only standard operations of a stack -- which means only push to 
+ * top, peek/pop from top, size, and is empty operations are valid.
+ * Depending on your language, stack may not be supported natively. You may 
+ * simulate a stack by using a list or deque (double-ended queue), as long as 
+ * you use only standard operations of a stack.
+ * 
+ * You may assume that all operations are valid (for example, no pop or peek 
+ * operations will be called on an empty queue).
+ *
+ ***************************************************************************
+ * {@link https://leetcode.com/problems/implement-queue-using-stacks/ }
+ */
+package _232_ImplementQueueUsingStack;
+
+import java.util.Stack;
+
+/** see test {@link _232_ImplementQueueUsingStack.SolutionTest } */
+public class Solution {
+
+    // container for newly pushed item
+    private Stack<Integer> pushStack = new Stack<>();
+
+    // container in which items are in 'queue' order
+    private Stack<Integer> peekStack = new Stack<>();
+
+    // Push element x to the back of queue.
+    public void push(int x) {
+        pushStack.push(x);
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        transfer();
+        peekStack.pop();
+    }
+
+    // Get the front element.
+    public int peek() {
+        transfer();
+        return peekStack.peek();
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return pushStack.isEmpty() && peekStack.isEmpty();
+    }
+
+    private void transfer() {
+        if (peekStack.isEmpty()) {
+            while (!pushStack.isEmpty()) {
+                peekStack.push(pushStack.pop());
+            }
+        }
+    }
+
+}
---
+++ java/test/_232_ImplementQueueUsingStack/SolutionTest.java
@@ -0,0 +1,61 @@
+package _232_ImplementQueueUsingStack;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+public class SolutionTest {
+    
+    /** Test method for {@link _232_ImplementQueueUsingStack.Solution } */
+    Solution solution;
+
+    @Rule
+    public Timeout globalTimeout = new Timeout(200);
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    @Test
+    public void Test1() {
+        solution.push(1);
+        solution.push(2);
+        assertEquals(1, solution.peek());
+        solution.push(3);
+        assertEquals(1, solution.peek());
+    }
+
+    @Test
+    public void Test2() {
+        assertTrue(solution.empty());
+        solution.push(1);
+        solution.push(2);
+        assertTrue(!solution.empty());
+        solution.push(3);
+        assertTrue(!solution.empty());
+    }
+
+    @Test
+    public void Test3() {
+        solution.push(1);
+        solution.push(2);
+        solution.pop();
+        assertEquals(2, solution.peek());
+        solution.push(3);
+        solution.pop();
+        assertEquals(3, solution.peek());
+        solution.pop();
+        assertTrue(solution.empty());
+    }
+
+}

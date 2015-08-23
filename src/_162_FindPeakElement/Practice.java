cc9be947b45c4107bf0f86eaf499bcc2e3773a40
Steven Cooks <stevenjoy1113@gmail.com>
2015-08-23 09:21:47 -0400

    162 (1) find peak element

---
+++ src/_162_FindPeakElement/Practice.java
@@ -0,0 +1,30 @@
+/**
+ ***************************************************************************
+ * Description:
+ * 
+ * A peak element is an element that is greater than its neighbors. 
+ * Given an input array where num[i] ≠ num[i+1], find a peak element and 
+ * return its index. 
+ * The array may contain multiple peaks, in that case return the index to 
+ * any one of the peaks is fine. 
+ * 
+ * You may imagine that num[-1] = num[n] = -∞. 
+ * 
+ * For example, in array [1, 2, 3, 1], 3 is a peak element and your 
+ * function should return the index number 2.
+ * 
+ ***************************************************************************
+ * @tag : Array; Binary Search
+ * {@link https://leetcode.com/problems/find-peak-element/ }
+ */
+package _162_FindPeakElement;
+
+/** see test {@link _162_FindPeakElement.PracticeTest } */
+public class Practice {
+
+    public int findPeakElement(int[] nums) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
---
+++ test/_162_FindPeakElement/PracticeTest.java
@@ -0,0 +1,73 @@
+package _162_FindPeakElement;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+public class PracticeTest {
+
+    /** Test method for {@link _162_FindPeakElement.Practice } */
+    Practice solution;
+
+    @Rule
+    public Timeout globalTimeout = new Timeout(200);
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Practice();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    @Test
+    public void Test0() {
+        int[] nums = { 1, 2, 0 };
+        int actual = solution.findPeakElement(nums);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test1() {
+        int[] nums = { 1, 2, 3, 1 };
+        int actual = solution.findPeakElement(nums);
+        int expected = 2;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test2() {
+        int[] nums = { 1, 2, 3, 4, 10, 9, 5, 9, 5, 9, 5, 10, 9, 8, 7, 5 };
+        int actual = solution.findPeakElement(nums);
+        Set<Integer> exps = new HashSet<>(Arrays.asList(4, 7, 9, 11));
+        assertTrue(exps.contains(actual));
+    }
+
+    @Test
+    public void Test3() {
+        int[] nums = { 1, 2, 1, 3, 4, 5, 7, 6 };
+        int actual = solution.findPeakElement(nums);
+        Set<Integer> exps = new HashSet<>(Arrays.asList(1, 6));
+        assertTrue(exps.contains(actual));
+    }
+
+    @Test
+    public void Test4() {
+        int[] nums = { 1, 10, 9, 8, 7, 6, 5, 4 };
+        int actual = solution.findPeakElement(nums);
+        Set<Integer> exps = new HashSet<>(Arrays.asList(1));
+        assertTrue(exps.contains(actual));
+    }
+
+}

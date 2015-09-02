56f849d98cf5030e3be3b797a0b5236b605f6fad
Steven Cooks <stevenjoy1113@gmail.com>
2015-09-02 10:41:36 -0400

    023 (2) priority queue solution

---
+++ src/_023_MergeKSortedLists/Practice.java
@@ -0,0 +1,24 @@
+/**
+ ***************************************************************************
+ * Description:
+ *
+ * Merge k sorted linked lists and return it as one sorted list. Analyze 
+ * and describe its complexity.
+ * 
+ ***************************************************************************
+ * @tag : Divide and Conquer; Linked List; Heap
+ * {@link https://leetcode.com/problems/merge-k-sorted-lists/ }
+ */
+package _023_MergeKSortedLists;
+
+import com.leetcode.ListNode;
+
+/** see test {@link _023_MergeKSortedLists.PracticeTest } */
+public class Practice {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ test/_023_MergeKSortedLists/PracticeTest.java
@@ -0,0 +1,71 @@
+package _023_MergeKSortedLists;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import com.leetcode.ListNode;
+
+public class PracticeTest {
+    
+    /** Test method for {@link _023_MergeKSortedLists.Practice } */
+    Practice solution;
+
+    @Rule
+    public Timeout globalTimeout = new Timeout(200);
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Practice();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    @Test
+    public void Test1() {
+        ListNode[] lists = {};
+        ListNode actual = solution.mergeKLists(lists);
+        ListNode expected = null;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test2() {
+        ListNode h1 = ListNode.constructLinkedList(Arrays.asList(1, 3, 5));
+        ListNode h2 = ListNode.constructLinkedList(Arrays.asList(2, 4, 6));
+        ListNode[] lists = {h1, h2};
+        ListNode actual = solution.mergeKLists(lists);
+        ListNode expected = ListNode.constructLinkedList(Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertTrue(ListNode.isSameList(actual, expected));
+    }
+
+    @Test
+    public void Test3() {
+        ListNode h1 = ListNode.constructLinkedList(Arrays.asList(-2, -1, -1, -1));
+        ListNode h2 = null;
+        ListNode[] lists = {h1, h2};
+        ListNode actual = solution.mergeKLists(lists);
+        ListNode expected = ListNode.constructLinkedList(Arrays.asList(-2, -1, -1, -1));
+        assertTrue(ListNode.isSameList(actual, expected));
+    }
+
+    @Test
+    public void Test4() {
+        ListNode h1 = ListNode.constructLinkedList(Arrays.asList(3));
+        ListNode h2 = ListNode.constructLinkedList(Arrays.asList(1));
+        ListNode[] lists = {h1, h2};
+        ListNode actual = solution.mergeKLists(lists);
+        ListNode expected = ListNode.constructLinkedList(Arrays.asList(1, 3));
+        assertTrue(ListNode.isSameList(actual, expected));
+    }
+
+}

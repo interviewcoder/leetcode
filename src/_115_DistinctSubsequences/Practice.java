418cb75a4e62ca96bd8290cab2203dfd3be76c31
Steven Cooks <stevenjoy1113@gmail.com>
2015-09-08 13:29:52 -0400

    115 (3) add practice and its test

---
+++ src/_115_DistinctSubsequences/Practice.java
@@ -0,0 +1,29 @@
+/**
+ ********************************************************
+ * Description:
+ * 
+ * Given a string S and a string T, count the number of distinct subsequences 
+ * of T in S. 
+ * A subsequence of a string is a new string which is formed from the original 
+ * string by deleting some (can be none) of the characters without disturbing 
+ * the relative positions of the remaining characters. (ie, "ACE" is a 
+ * subsequence of "ABCDE" while "AEC" is not). 
+ * 
+ * Here is an example: S = "rabbbit", T = "rabbit" 
+ * Return 3.
+ * 
+ ********************************************************
+ * @tag : Dynamic Programming; String
+ * {@link https://leetcode.com/problems/distinct-subsequences/ }
+ */
+package _115_DistinctSubsequences;
+
+/** see test {@link _115_DistinctSubsequences.PracticeTest } */
+public class Practice {
+
+    public int numDistinct(String s, String t) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
---
+++ test/_115_DistinctSubsequences/PracticeTest.java
@@ -0,0 +1,125 @@
+package _115_DistinctSubsequences;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+public class PracticeTest {
+    
+    /** Test method for {@link _115_DistinctSubsequences.Practice } */
+    Practice solution;
+
+    @Rule
+    public Timeout globalTimeout = new Timeout(200);
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Practice();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    @Test
+    public void Test1() {
+        String s = "rabbbit";
+        String t = "rabbit";
+        int actual = solution.numDistinct(s, t);
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test2() {
+        String s = "a";
+        String t = "";
+        int actual = solution.numDistinct(s, t);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test3() {
+        String s = "ababab";
+        String t = "ab";
+        int actual = solution.numDistinct(s, t);
+        int expected = 6;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test4() {
+        String s = "abcb";
+        String t = "abcb";
+        int actual = solution.numDistinct(s, t);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test5() {
+        String s = "adbdadeecadeadeccaeaabdabdbcdabddddabcaaadbabaaedeedd"
+                + "eaeebcdeabcaaaeeaeeabcddcebddebeebedaecccbdcbcedbdaeae"
+                + "dcdebeecdaaedaacadbdccabddaddacdddc";
+        String t = "bcddceeeebecbc";
+        int actual = solution.numDistinct(s, t);
+        int expected = 700531452;
+        assertEquals(expected, actual);
+    }
+    
+
+    @Test
+    public void Test6() {
+        String s = "anacondastreetracecar";
+        String t = "contra";
+        int actual = solution.numDistinct(s, t);
+        int expected = 6;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test7() {
+        String s = "daacaedaceacabbaabdccdaaeaebacddadcaeaacadbceaecddecd"
+                + "eedcebcdacdaebccdeebcbdeaccabcecbeeaadbccbaeccbbdaeade"
+                + "cabbbedceaddcdeabbcdaeadcddedddcececbeeabcbecaeadddedd"
+                + "ccbdbcdcbceabcacddbbcedebbcaccac";
+        String t = "ceadbaa";
+        int actual = solution.numDistinct(s, t);
+        int expected = 8556153;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test8() {
+        String s = "xslledayhxhadmctrliaxqpokyezcfhzaskeykchkmhpyjipxtsul"
+                + "jkwkovmvelvwxzwieeuqnjozrfwmzsylcwvsthnxujvrkszqwtglew"
+                + "kycikdaiocglwzukwovsghkhyidevhbgffoqkpabthmqihcfxxzdej"
+                + "letqjoxmwftlxfcxgxgvpperwbqvhxgsbbkmphyomtbjzdjhcrcsgg"
+                + "leiczpbfjcgtpycpmrjnckslrwduqlccqmgrdhxolfjafmsrfdghna"
+                + "texyanldrdpxvvgujsztuffoymrfteholgonuaqndinadtumnuhkboy"
+                + "zaqguwqijwxxszngextfcozpetyownmyneehdwqmtpjloztswmzzdz"
+                + "qhuoxrblppqvyvsqhnhryvqsqogpnlqfulurexdtovqpqkfxxnqykg"
+                + "scxaskmksivoazlducanrqxynxlgvwonalpsyddqmaemcrrwvrjmjj"
+                + "nygyebwtqxehrclwsxzylbqexnxjcgspeynlbmetlkacnnbhmaizba"
+                + "dynajpibepbuacggxrqavfnwpcwxbzxfymhjcslghmajrirqzjqxpg"
+                + "tgisfjreqrqabssobbadmtmdknmakdigjqyqcruujlwmfoagrckdwy"
+                + "iglviyyrekjealvvigiesnvuumxgsveadrxlpwetioxibtdjblowbl"
+                + "qvzpbrmhupyrdophjxvhgzclidzybajuxllacyhyphssvhcffxonys"
+                + "ahvzhzbttyeeyiefhunbokiqrpqfcoxdxvefugapeevdoakxwzykmh"
+                + "bdytjbhigffkmbqmqxsoaiomgmmgwapzdosorcxxhejvgajyzdmzlc"
+                + "ntqbapbpofdjtulstuzdrffafedufqwsknumcxbschdybosxkrabyf"
+                + "dejgyozwillcxpcaiehlelczioskqtptzaczobvyojdlyflilvwqgy"
+                + "rqmjaeepydrcchfyftjighntqzoo"; 
+        String t =  "rwmimatmhydhbujebqehjprrwfkoebcxxqfktayaaeheys";
+        int actual = solution.numDistinct(s, t);
+        int expected = 543744000;
+        assertEquals(expected, actual);
+    }
+
+}

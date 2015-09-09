74742a9f5d5509e8e6438fa5273643e0a376df74
Steven Cooks <stevenjoy1113@gmail.com>
2015-09-08 23:18:56 -0400

    update lib

--- src/com/leetcode/TreeNode.java
+++ src/com/leetcode/TreeNode.java
@@ -186,6 +186,26 @@ public class TreeNode {
     public static TreeNode getBST6() {
         return getTree11();
     }
+
+    //       4 
+    //     /    \
+    //    2      6 
+    //   / \    / \
+    //  1   3  5   7 
+    public static TreeNode getBST7() {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode.connect(n4, n2, n6);
+        TreeNode.connect(n2, n1, n3);
+        TreeNode.connect(n6, n5, n7);
+        TreeNode root = n4;
+        return root;
+    }
     
     // 1 
     public static TreeNode getTree0() {

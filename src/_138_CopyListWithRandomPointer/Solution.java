dbe94253db4af2e64cab873dccb3c7dca756ea68
Steven Cooks <stevenjoy1113@gmail.com>
2015-09-10 11:57:57 -0400

    138 (3) update DFS solution

--- src/_138_CopyListWithRandomPointer/Solution.java
+++ src/_138_CopyListWithRandomPointer/Solution.java
@@ -24,6 +24,7 @@ public class Solution {
     
     private Map<RandomListNode, RandomListNode> randomMap = new HashMap<>();
     
+    // DFS version
     public RandomListNode copyRandomList(RandomListNode head) {
         if (head == null) {
             return head;
@@ -35,10 +36,12 @@ public class Solution {
         // construct a new node
         RandomListNode copyHead = new RandomListNode(head.label);
         randomMap.put(head, copyHead);
+
         // copy next pointer
         copyHead.next = copyRandomList(head.next);
         // copy random pointer
         copyHead.random = copyRandomList(head.random);
+
         return copyHead;
     }
 }

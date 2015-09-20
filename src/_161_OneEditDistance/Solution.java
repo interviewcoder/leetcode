53aa14b32f6f99b427db537c85466facdd23e26a
Steven Cooks <stevenjoy1113@gmail.com>
2015-09-20 16:06:52 -0400

    161 (2) update solution

--- src/_161_OneEditDistance/Solution.java
+++ src/_161_OneEditDistance/Solution.java
@@ -15,11 +15,31 @@ package _161_OneEditDistance;
 
 /** see test {@link _161_OneEditDistance.SolutionTest } */
 public class Solution {
-
+    
     // one edit distance:
     // 1) equal length: only one substitution
     // 2) lengths diff by 1: skip one on longer string, then rest should be the same 
     public boolean isOneEditDistance(String s, String t) {
+        if (s.length() > t.length()) {
+            return isOneEditDistance(t, s);
+        }
+        if (s.length() + 1 < t.length()) {
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != t.charAt(i)) {
+                if (s.length() == t.length()) {
+                    return s.substring(i + 1).equals(t.substring(i + 1));
+                } else {
+                    return s.substring(i).equals(t.substring(i + 1));
+                }
+            }
+        }
+        // no different characters found so far then they must have different length
+        return s.length() < t.length();
+    }
+
+    public boolean isOneEditDistance_Verbose(String s, String t) {
         if (s.length() > t.length()) {
             return isOneEditDistance(t, s);
         }

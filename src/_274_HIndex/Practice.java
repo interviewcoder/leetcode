ec101adb800312ee98663d75830988945d049f06
Steven Cooks <stevenjoy1113@gmail.com>
2015-09-21 12:10:35 -0400

    274 (1) hash table + bucket sort

---
+++ src/_274_HIndex/Practice.java
@@ -0,0 +1,35 @@
+/**
+ * Time : O(N) ; Space: O(N)
+ * @tag : Hash Table; Sort
+ * @by  : Steven Cooks
+ * @date: Sep 21, 2015
+ ***************************************************************************
+ * Description:
+ * 
+ * Given an array of citations (each citation is a non-negative integer) of 
+ * a researcher, write a function to compute the researcher's h-index. 
+ * According to the definition of h-index on Wikipedia: "A scientist has 
+ * index h if h of his/her N papers have at least h citations each, and the 
+ * other N − h papers have no more than h citations each." 
+ * 
+ * For example, given citations = [3, 0, 6, 1, 5], which means the researcher 
+ * has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations 
+ * respectively. Since the researcher has 3 papers with at least 3 citations 
+ * each and the remaining two with no more than 3 citations each, his h-index is 3. 
+ * 
+ * Note: If there are several possible values for h, the maximum one is taken as the h-index. *
+ * 
+ ***************************************************************************
+ * {@link https://leetcode.com/problems/h-index/ }
+ */
+package _274_HIndex;
+
+/** see test {@link _274_HIndex.PracticeTest} */
+public class Practice {
+
+    public int hIndex(int[] nums) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
---
+++ test/_274_HIndex/SolutionTest.java
@@ -0,0 +1,45 @@
+package _274_HIndex;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+public class SolutionTest {
+    
+    /** Test method for {@link _274_HIndex.Solution } */
+    Solution solution;
+
+    @Rule
+    public Timeout globalTimeout = new Timeout(200);
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    @Test
+    public void Test1() {
+        int[] nums = { 0 };
+        int actual = solution.hIndex(nums);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test2() {
+        int[] nums = { 3, 1, 0, 6, 5 };
+        int actual = solution.hIndex(nums);
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+
+}

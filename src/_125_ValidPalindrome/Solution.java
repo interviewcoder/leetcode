0f58392308a30640122fcbd53dd58be0a6f693f2
Steven Cooks <stevenjoy1113@gmail.com>
2015-09-21 23:11:59 -0400

    125 (3) both while and nested-while solutions

--- src/_125_ValidPalindrome/Solution.java
+++ src/_125_ValidPalindrome/Solution.java
@@ -1,15 +1,18 @@
 /**
- * Time : O(); Space: O()
+ * Time : O(N); Space: O(1)
  * @tag : Two Pointers; String
  * @date: Jun 16, 2015
  * @by  : Steven Cooks
  *************************************************************************
+ * Description:
+ * 
  *  Given a string, determine if it is a palindrome, considering only 
  *  alphanumeric characters and ignoring cases. 
  *  
  *  For example, 
  *  "A man, a plan, a canal: Panama" is a palindrome. 
  *  "race a car" is not a palindrome.
+ *  
  *************************************************************************
  * {@link https://leetcode.com/problems/valid-palindrome/ }
  */
@@ -17,15 +20,34 @@ package _125_ValidPalindrome;
 
 /** see test {@link _125_ValidPalindrome.SolutionTest } */
 public class Solution {
+
     public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            } else if (!Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            } else { 
+                if (Character.toLowerCase(s.charAt(i)) != Character
+                        .toLowerCase(s.charAt(j))) {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+
+    // while version
+    public boolean isPalindrome_while(String s) {
         int len = s.length();
         // corner case
-        if (len == 0) {
-            return true;
-        }
         int i = 0;
         int j = len - 1;
-        while (i <= j) {
+        while (i < j) {
             // find the next non-empty character from the beginning
             while (i <= j && !Character.isLetterOrDigit(s.charAt(i))) {
                 i++;
@@ -35,7 +57,9 @@ public class Solution {
                 j--;
             }
             // compare
-            if (i <= j && Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+            if (i <= j
+                    && Character.toLowerCase(s.charAt(i)) != Character
+                            .toLowerCase(s.charAt(j))) {
                 return false;
             }
             i++;
@@ -43,4 +67,5 @@ public class Solution {
         }
         return true;
     }
+
 }

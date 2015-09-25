e8ad4659172ce83174e79d916f55fdabea7317bd
Steven Cooks <stevenjoy1113@gmail.com>
2015-09-25 00:20:34 -0400

    067 (3) add concise solution

--- src/_067_AddBinary/Solution.java
+++ src/_067_AddBinary/Solution.java
@@ -18,33 +18,20 @@ package _067_AddBinary;
 
 /** see test {@link _067_AddBinary.SolutionTest } */
 public class Solution {
-    public String addBinary(String a, String b) {
-        if (a.length() < b.length()) {
-            return addBinary(b, a);
-        }
-        String result = "";
-        int carry = 0;
-        int radix = 2;
-        int lenA = a.length();
-        int lenB = b.length();
-        for (int i = lenB - 1; i >= 0; i--) {
-            char chA = a.charAt(i + lenA - lenB);
-            char chB = b.charAt(i);
-            int number = (chA - '0') + (chB - '0') + carry;
-            result = (number % radix) + result;
-            carry = number / radix;
-        }
-        // the rest part of string a
-        for (int i = lenA - lenB - 1; i >= 0; i--) {
-            int number = (a.charAt(i) - '0') + carry;
-            result = (number % radix) + result;
-            carry = number / radix;
-        }
-        // deal with unfinished carry
-        if (carry != 0) {
-            result = "1" + result;
-        }
 
-        return result;
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int num1 = i >= 0 ? a.charAt(i--) - '0' : 0;
+            int num2 = j >= 0 ? b.charAt(j--) - '0' : 0;
+            int num = num1 + num2 + carry;
+            sb.append(num % 2);
+            carry = num / 2;
+        }
+        return sb.reverse().toString();
     }
+
 }

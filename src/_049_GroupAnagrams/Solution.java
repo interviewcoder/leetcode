61e9cfd65474b0b70762c840c87a3f29f6dc78a6
Steven Cooks <stevenjoy1113@gmail.com>
2015-09-27 18:38:21 -0400

    049 (3) add version2 solution

--- src/_049_GroupAnagrams/Solution.java
+++ src/_049_GroupAnagrams/Solution.java
@@ -18,6 +18,7 @@ package _049_GroupAnagrams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -25,26 +26,47 @@ import java.util.Map;
 public class Solution {
 
     /**
-     * For each string, sort it alphabetically, and then check whether this 
+     * For each string, sort it alphabetically, and then check whether this
      * sorted string has appeared in map.
      */
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> result = new ArrayList<>();
-        // <sorted string, index of list in result list> 
+        // <sorted string, index of list in result list>
         Map<String, Integer> anagrams = new HashMap<>();
         Arrays.sort(strs);
         for (String str : strs) {
             char[] chars = str.toCharArray();
             Arrays.sort(chars);
             String sorted = new String(chars);
-            if (anagrams.containsKey(sorted)) {
-                result.get(anagrams.get(sorted)).add(str);
-            } else {
+            if (!anagrams.containsKey(sorted)) {
                 anagrams.put(sorted, result.size());
-                result.add(new ArrayList<>(Arrays.asList(str)));
+                result.add(new ArrayList<>());
             }
+            result.get(anagrams.get(sorted)).add(str);
         }
         return result;
+    }
+    
+    
+    // version 2: larger map, not as space efficient as version1
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        Arrays.sort(strs);
+        // <sorted array, list of anagrams>
+        Map<String, List<String>> anagrams = new HashMap<>();
+        for (String s : strs) {
+            char[] str = s.toCharArray();
+            Arrays.sort(str);
+            String anagram = new String(str);
+            if (!anagrams.containsKey(anagram)) {
+                anagrams.put(anagram, new LinkedList<>());
+            }
+            anagrams.get(anagram).add(s);
+        }
+        for (List<String> list : anagrams.values()) {
+            res.add(list);
+        }
+        return res;
     }
 
 }

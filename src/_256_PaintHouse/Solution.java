5f55edd614704f4986e5edd5c3e0363e09417492
Steven Cooks <stevenjoy1113@gmail.com>
2015-09-28 21:46:26 -0400

    256 (1) min matrix dp

---
+++ src/_256_PaintHouse/Solution.java
@@ -0,0 +1,53 @@
+/**
+ * Time : O() ; Space: O()
+ * @tag : Dynamic Programming
+ * @by  : Steven Cooks
+ * @date: Sep 28, 2015
+ ***************************************************************************
+ * Description:
+ * 
+ * There are a row of n houses, each house can be painted with one of the 
+ * three colors: red, blue or green. The cost of painting each house with 
+ * a certain color is different. You have to paint all the houses such that 
+ * no two adjacent houses have the same color. 
+ * 
+ * The cost of painting each house with a certain color is represented by a 
+ * n x 3 cost matrix. For example, costs[0][0] is the cost of painting 
+ * house 0 with color red; costs[1][2] is the cost of painting house 1 with 
+ * color green, and so on... Find the minimum cost to paint all houses.
+ * 
+ * Note: All costs are positive integers.
+ * 
+ ***************************************************************************
+ * {@link https://leetcode.com/problems/paint-house/ }
+ */
+package _256_PaintHouse;
+
+/** see test {@link _256_PaintHouse.SolutionTest } */
+public class Solution {
+
+    public int minCost(int[][] costs) {
+        int n = costs.length;
+        if (n == 0) {
+            return 0;
+        }
+        int f[][] = new int[n + 1][costs[0].length];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int c = 2; c >= 0; c--) {
+                int mincost = Integer.MAX_VALUE;
+                for (int color = 0; color <= 2; color++) {
+                    if (color != c) {
+                        mincost = Math.min(mincost, f[i + 1][color]);
+                    }
+                }
+                f[i][c] = mincost + costs[i][c];
+            }
+        }
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < f[0].length; i++) {
+            res = Math.min(res, f[0][i]);
+        }
+        return res;
+    }
+
+}
---
+++ test/_256_PaintHouse/PracticeTest.java
@@ -0,0 +1,110 @@
+package _256_PaintHouse;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+public class PracticeTest {
+
+    /** Test method for {@link _256_PaintHouse.Practice } */
+    Practice solution;
+
+    @Rule
+    public Timeout globalTimeout = new Timeout(200);
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Practice();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        solution = null;
+    }
+
+    @Test
+    public void Test0() {
+        int[][] costs = {
+                { 10, 30, 20 },
+        };
+        int actual = solution.minCost(costs);
+        int expected = 10;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test1() {
+        int[][] costs = {
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 }
+        };
+        int actual = solution.minCost(costs);
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test2() {
+        int[][] costs = {
+                { 1, 2, 3 },
+                { 3, 2, 1 },
+                { 2, 2, 2 },
+                { 3, 1, 2 }
+        };
+        int actual = solution.minCost(costs);
+        int expected = 5;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test3() {
+        int[][] costs = {
+                {17,  2, 17 },
+                {16, 16,  5 },
+                {14,  3, 19 },
+        };
+        int actual = solution.minCost(costs);
+        int expected = 10;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test4() {
+        int[][] costs = { 
+                { 12,  1, 19 }, 
+                { 15,  1, 10 }, 
+                {  3, 11, 10 },
+                {  9,  3, 10 }, 
+                {  4,  8,  7 }, 
+                {  4, 18,  2 }, 
+                { 16,  6,  6 },
+                {  3,  3,  6 }, 
+                { 10, 18, 16 }, 
+                {  5,  4,  8 }, 
+                {  5,  3, 16 },
+                { 11,  8, 19 }, 
+                { 18, 15, 18 }, 
+                { 16,  4, 15 }, 
+                { 10,  7, 13 },
+                { 11, 10, 14 }, 
+                {  3,  9,  8 }, 
+                {  5,  2,  2 }, 
+                {  3,  2,  5 },
+                {  2, 19, 14 }, 
+                { 17,  3,  6 }, 
+                {  6,  4, 17 }, 
+                {  5, 15, 19 },
+                {  2, 14, 14 }, 
+                { 19,  4, 16 } 
+        };
+        int actual = solution.minCost(costs);
+        int expected = 143;
+        assertEquals(expected, actual);
+    }
+
+}

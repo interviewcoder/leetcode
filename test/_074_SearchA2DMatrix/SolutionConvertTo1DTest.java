66a51b265a961079b2c72b2996c3ebe95d280cf9
Steven Cooks <stevenjoy1113@gmail.com>
2015-10-29 12:01:36 -0400

    074 (2) update tests

--- test/_074_SearchA2DMatrix/SolutionConvertTo1DTest.java
+++ test/_074_SearchA2DMatrix/SolutionConvertTo1DTest.java
@@ -28,8 +28,11 @@ public class SolutionConvertTo1DTest {
 
     @Test
     public void Test1() {
-        int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 },
-                { 23, 30, 34, 50 } };
+        int[][] matrix = { 
+                {  1,  3,  5,  7 }, 
+                { 10, 11, 16, 20 },
+                { 23, 30, 34, 50 } 
+        };
         int target = 3;
         boolean actual = solution.searchMatrix(matrix, target);
         boolean expected = true;
@@ -38,8 +41,11 @@ public class SolutionConvertTo1DTest {
 
     @Test
     public void Test2() {
-        int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 },
-                { 23, 30, 34, 50 } };
+        int[][] matrix = { 
+                {  1,  3,  5,  7 }, 
+                { 10, 11, 16, 20 },
+                { 23, 30, 34, 50 } 
+        };
         int target = 100;
         boolean actual = solution.searchMatrix(matrix, target);
         boolean expected = false;
@@ -48,8 +54,11 @@ public class SolutionConvertTo1DTest {
 
     @Test
     public void Test3() {
-        int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 },
-                { 23, 30, 34, 50 } };
+        int[][] matrix = { 
+                {  1,  3,  5,  7 }, 
+                { 10, 11, 16, 20 },
+                { 23, 30, 34, 50 } 
+        };
         int target = 15;
         boolean actual = solution.searchMatrix(matrix, target);
         boolean expected = false;
@@ -58,8 +67,11 @@ public class SolutionConvertTo1DTest {
 
     @Test
     public void Test4() {
-        int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 },
-                { 23, 30, 34, 50 } };
+        int[][] matrix = { 
+                {  1,  3,  5,  7 }, 
+                { 10, 11, 16, 20 },
+                { 23, 30, 34, 50 } 
+        };
         int target = 30;
         boolean actual = solution.searchMatrix(matrix, target);
         boolean expected = true;
@@ -68,8 +80,11 @@ public class SolutionConvertTo1DTest {
 
     @Test
     public void Test5() {
-        int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 },
-                { 23, 30, 34, 50 } };
+        int[][] matrix = { 
+                {  1,  3,  5,  7 }, 
+                { 10, 11, 16, 20 },
+                { 23, 30, 34, 50 } 
+        };
         int target = 0;
         boolean actual = solution.searchMatrix(matrix, target);
         boolean expected = false;
@@ -78,12 +93,127 @@ public class SolutionConvertTo1DTest {
 
     @Test
     public void Test6() {
-        int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 },
-                { 23, 30, 34, 50 } };
+        int[][] matrix = { 
+                {  1,  3,  5,  7 }, 
+                { 10, 11, 16, 20 },
+                { 23, 30, 34, 50 } 
+        };
         int target = 10;
         boolean actual = solution.searchMatrix(matrix, target);
         boolean expected = true;
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test7() {
+        int[][] matrix = { {  1,  3,  5,  7 } };
+        int target = 10;
+        assertFalse(solution.searchMatrix(matrix, target));
+    }
+
+    @Test
+    public void Test8() {
+        int[][] matrix = { {  1,  3,  5,  7 } };
+        int target = 7;
+        assertTrue(solution.searchMatrix(matrix, target));
+    }
+
+    @Test
+    public void Test9() {
+        int[][] matrix = { {  1,  3,  5,  7 } };
+        int target = 1;
+        assertTrue(solution.searchMatrix(matrix, target));
+    }
+
+    @Test
+    public void Test10() {
+        int[][] matrix = { {  1,  3,  5,  7 } };
+        int target = 4;
+        assertFalse(solution.searchMatrix(matrix, target));
+    }
+
+    @Test
+    public void Test11() {
+        int[][] matrix = { {  1,  3,  5,  7 } };
+        int target = 5;
+        assertTrue(solution.searchMatrix(matrix, target));
+    }
+
+    @Test
+    public void Test12() {
+        int[][] matrix = { 
+                { 1 },
+                { 5 },
+                { 9 },
+        };
+        int target = 9;
+        assertTrue(solution.searchMatrix(matrix, target));
+    }
+
+    @Test
+    public void Test13() {
+        int[][] matrix = { 
+                { 1 },
+                { 5 },
+                { 9 },
+        };
+        int target = 1;
+        assertTrue(solution.searchMatrix(matrix, target));
+    }
+
+    @Test
+    public void Test14() {
+        int[][] matrix = { 
+                { 1 },
+                { 5 },
+                { 9 },
+        };
+        int target = 5;
+        assertTrue(solution.searchMatrix(matrix, target));
+    }
+
+    @Test
+    public void Test15() {
+        int[][] matrix = { 
+                { 1 },
+                { 5 },
+                { 9 },
+        };
+        int target = 18;
+        assertFalse(solution.searchMatrix(matrix, target));
+    }
+
+    @Test
+    public void Test16() {
+        int[][] matrix = { 
+                { 1 },
+                { 5 },
+                { 9 },
+        };
+        int target = 0;
+        assertFalse(solution.searchMatrix(matrix, target));
+    }
+
+    @Test
+    public void Test17() {
+        int[][] matrix = { 
+                { 1 },
+                { 5 },
+                { 9 },
+        };
+        int target = 3;
+        assertFalse(solution.searchMatrix(matrix, target));
+    }
+
+    @Test
+    public void Test18() {
+        int[][] matrix = { 
+                { 1 },
+                { 5 },
+                { 9 },
+        };
+        int target = 7;
+        assertFalse(solution.searchMatrix(matrix, target));
     }
 
 }

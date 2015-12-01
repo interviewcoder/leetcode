3aad8d8fbeced3fd75d3bbb2d04b354c6b743717
Steven Cooks <stevenjoy1113@gmail.com>
2015-12-01 15:48:06 -0500

    002 (3) update concise solution

--- src/_002_AddTwoNumbers/Solution.java
+++ src/_002_AddTwoNumbers/Solution.java
@@ -8,7 +8,7 @@
  * 
  * You are given two linked lists representing two non-negative numbers. 
  * The digits are stored in reverse order and each of their nodes contain 
- * a single digit. Add the two numbers and return it as a linked list. 
+ * l1 single digit. Add the two numbers and return it as l1 linked list. 
  * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) 
  * Output: 7 -> 0 -> 8
  * 
@@ -28,26 +28,18 @@ public class Solution {
      *  Be careful to test null before reference node's value
      */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carry = 0;
-        ListNode h1 = l1;
-        ListNode h2 = l2;
         ListNode dummy = new ListNode(0);
         ListNode node = dummy;
-        while (h1 != null || h2 != null || carry > 0) {
-            int n1 = 0;
-            int n2 = 0;
-            if (h1 != null) {
-                n1 = h1.val;
-                h1 = h1.next;
-            }
-            if (h2 != null) {
-                n2 = h2.val;
-                h2 = h2.next;
-            }
-            int num = n1 + n2 + carry;
-            node.next = new ListNode(num % 10);
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            node.next = new ListNode(sum % 10);
             node = node.next;
-            carry = num / 10;
+            carry = sum / 10;
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
         }
         return dummy.next;
     }
